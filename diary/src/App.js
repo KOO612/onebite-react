413d1c876786eec7945a817c6dd72ac23bb93b79
KOO612 <ksm99007@gmail.com>
2023-11-28 18:04:19 +0900

    감정일기 1일차

---
+++ diary/src/App.js
@@ -0,0 +1,27 @@
+import { Link, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import New from './pages/New';
+import Diary from './pages/Diary';
+import Edit from './pages/Edit';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/new" element={<New />}></Route>
+        <Route path="/diary/:id" element={<Diary />}></Route>
+        <Route path="/edit" element={<Edit />}></Route>
+      </Routes>
+      <div>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/new'}>New</Link>
+        <Link to={'/diary'}>Diary</Link>
+        <Link to={'/edit'}>Edit</Link>
+      </div>
+    </div>
+  );
+}
+
+export default App;
